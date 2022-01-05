571e68a917ebae0f7c8eca0dcd9b667717970087
Dongd <choise154@gmail.com>
2022-01-05 10:53:28 +0900

    BOJ 2775.부녀회장이 될테야 다시 풀기

--- BOJ/2775.부녀회장이 될테야/Main.java
+++ BOJ/2775.부녀회장이 될테야/Main.java
@@ -3,10 +3,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    
+        
+    public static int K;
+    public static int N;
+    public static int[] answer;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        // 따로 다시풀기
+        int T = Integer.parseInt(br.readLine());
+        answer = new int[T];
+        for(int t=0; t<T; t++) {
+            K = Integer.parseInt(br.readLine());
+            N = Integer.parseInt(br.readLine());
+            int[][] apt = getAPT();
+            answer[t] = apt[K][N];
+        }
+        for(int t=0; t<T; t++) {
+            System.out.println(answer[t]);
+        }
+    }
+    public static int[][] getAPT() {
+        int[][] result = new int[K+1][N+1];
+        for(int i=0; i<=K; i++) {
+            result[i][0] = 0;
+            for(int j=1; j<=N; j++) {
+                if(i == 0) { result[i][j] = j; }
+                else {
+                    result[i][j] = result[i-1][j] + result[i][j-1];
+                }
+            }
+        }
+        return result;
     }
 }
