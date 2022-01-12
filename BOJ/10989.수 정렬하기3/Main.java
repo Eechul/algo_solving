09e7fb934acd40ed581ade561049bbc687fd9d8d
DONG-PC <choise154@gmail.com>
2022-01-12 23:29:01 +0900

    수 정렬하기3 시간초과 문제 해결

--- BOJ/10989.수 정렬하기3/Main.java
+++ BOJ/10989.수 정렬하기3/Main.java
@@ -8,34 +8,20 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
         int MAX_SIZE = 10001;
         int MAX_NUM = N+1;
-		int[] question = new int[N+1];
         int[] count = new int[MAX_SIZE];
         
-        int[] result = new int[N+1];
-        String resultStr = "";
+        StringBuilder resultSb = new StringBuilder();
 
         for(int i=1; i<MAX_NUM; i++) {
-            question[i] = Integer.parseInt(br.readLine());
-            count[question[i]]++;
-        }
-        int beforeIdx = 0;
-        for(int i=1; i<MAX_SIZE; i++) {
-            if(count[i] != 0) { 
-                if(beforeIdx != 0) {
-                    count[i] = count[i] + count[beforeIdx];
-                }
-                beforeIdx = i;
-            }
+            count[Integer.parseInt(br.readLine())]++;
         }
 
-        for(int j=N; j>0; j--) {
-            result[count[question[j]]] = question[j];
-            count[question[j]]--;
+        for(int j=1; j<MAX_SIZE; j++) {
+            while(count[j] > 0) {
+                resultSb.append(j).append('\n');
+                count[j]--;
+            }
         }
-        for(int i=1; i<N+1; i++) {
-            resultStr += result[i]+"\n";
-        }
-        System.out.println(resultStr);
-        
+        System.out.println(resultSb);
 	}
 }
--- BOJ/10989.수 정렬하기3/readme.md
+++ BOJ/10989.수 정렬하기3/readme.md
@@ -1,2 +1,3 @@
 [백준 - 10989.수 정렬하기
 ](https://www.acmicpc.net/problem/10989)
+> hint: 자연수 범위가 작다는것. 
