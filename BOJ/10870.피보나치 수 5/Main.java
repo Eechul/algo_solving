78e16e4a7c358637c164e96f5e4f867ca718444c
DONG-PC <choise154@gmail.com>
2022-01-17 22:25:04 +0900

    팩토리얼, 피보나치 수 5 해결

---
+++ BOJ/10870.피보나치 수 5/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(fivonacci(N));
+    }
+
+    public static int fivonacci(int n) {
+        if(n == 0) { return 0; }
+        if(n == 1) { return 1; }
+
+        return (fivonacci(n-1) + fivonacci(n-2));
+    }
+}
