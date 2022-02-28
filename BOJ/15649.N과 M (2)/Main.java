a8ed3dbbee80bce134e16d6f1ef32d8b9682b0a2
Dongd <choise154@gmail.com>
2022-02-28 11:19:22 +0900

    백트래킹 (bfs) N과 M(2) 문제 해결

---
+++ BOJ/15649.N과 M (2)/Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static StringBuilder sb = new StringBuilder();
+	static int N;
+	static int M;
+	static int[] arr;
+
+	public static void main(String[] args) throws Exception {		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[M];
+		
+		dfs(1, 0);
+		System.out.println(sb.toString());
+	}
+	
+	public static void dfs(int at, int depth) {
+		if(depth == M) {
+			for(int val : arr) {
+				sb.append(val); sb.append(" ");
+			}
+			sb.append("\n");
+			return ;
+		}
+
+		for(int i = at; i <= N; i++) {
+			arr[depth] = i;
+			dfs(i+1, depth+1);
+		}
+	}
+}
