b748fc58ea99271e40d120da774d314f3c3cd029
Dongd <choise154@gmail.com>
2022-07-15 13:41:10 +0900

    백준 10172.개 문제 해결

---
+++ BOJ/10172.개/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+	
+	public static void main(String[] args) throws Exception {		
+		StringBuilder sb = new StringBuilder();
+		sb.append("|\\_/|");
+		sb.append("\n");
+		sb.append("|q p|   /}");
+		sb.append("\n");
+		sb.append("( 0 )\"\"\"\\");
+		sb.append("\n");
+		sb.append("|\"^\"`    |");
+		sb.append("\n");
+		sb.append("||_/=\\\\__|");
+
+		System.out.println(sb.toString());		
+    }
+}
---
+++ BOJ/10172.개/readme.md
@@ -0,0 +1,3 @@
+[백준 - 10172.개
+](https://www.acmicpc.net/problem/10172)
+> note: 역시 고양이🐱 보단 강아지🐶가 귀엽다.
